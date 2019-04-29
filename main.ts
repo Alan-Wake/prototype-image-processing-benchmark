406a2ae39c5fdf4321d630b483628ce03cb36077
DongHyeon Yu <alan911011@gmail.com>
2019-04-29 12:34:38 +0900

    type: main.ts change - 사용하지 않는 코드 정리

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
-import { APIGatewayEvent, Context, Handler, Callback } from 'aws-lambda';
+import { Context, Handler, Callback } from 'aws-lambda';
 import * as Sharp from 'sharp';
 import * as AWS from 'aws-sdk';
 
 const s3 = new AWS.S3();
-
 interface IPreset {
   top: number;
   left: number;
@@ -11,7 +10,7 @@ interface IPreset {
   height: number;
 }
 
-export const buildRespose = (input: any, message: any, code: number): any => ({
+export const buildRespose = (input: any, message: any, code: number): any=> ({
   statusCode: code,
   headers: {
     'Access-Control-Allow-Origin': '*',
@@ -23,61 +22,47 @@ export const buildRespose = (input: any, message: any, code: number): any => ({
   }),
 })
 
-export const upload = (bucketName: string, key: string, buffer: Buffer, contentType: string): Promise<any> => {
-  return s3.putObject({
-    Bucket: bucketName,
-    Key: key,
-    ContentType: contentType,
-    ContentEncoding: "base64",
-    Body: buffer,
-  }).promise();
-}
-
 export const download = async (bucketName: string, key: string): Promise<any> => {
   return await s3.getObject({ Bucket: bucketName, Key: key }).promise();
-}
-
-export const parseBase64Buffer = (body: any): Buffer => {
-  return new Buffer(body.replace(/^data:image\/\w+;base64,/, ""), 'base64');
 }
 
 export const flatten = (arr: any, depth = 1) =>
   arr.reduce((a: any, v: any) => a.concat(depth > 1 && Array.isArray(v) ? flatten(v, depth - 1) : v), []);
 
-export const crop = async (image: Sharp.Sharp, preset: IPreset): Promise<Buffer> => {
-  try{
-    const retImage = await image.extract({ ...preset }).toBuffer();
-    await Sharp.cache(false);
-    return retImage;
-  }catch(e){
+export const crop = (image: Sharp.Sharp, preset: IPreset): Promise<Buffer> => {
+  try {
+    return image.extract({ ...preset }).toBuffer();
+  } catch (e) {
     throw new Error(e);
   }
 }
 
-export const uploadImage: Handler = async (event: any, context: Context, cb: any) => {
-  const { x, y, name } = event;
+export const uploadImage: Handler = async (event: any, context: Context, cb: Callback) => {
+  const { cols, rows, name } = event;
 
   try {
+    const hrstart = await process.hrtime();
     const file = await download('alanwake',name);
+    const hrend = await process.hrtime(hrstart);
     await Sharp.cache(false);
     const idata = await Sharp(file.Body).sequentialRead(true).limitInputPixels(0);
     const metadata = await idata.metadata();
-    const presets: IPreset[][] = [...Array(x).keys()].map(_x => {
-      return [...Array(y).keys()].map(_y => {
+    const presets: IPreset[][] = [...Array(cols).keys()].map(col => {
+      return [...Array(rows).keys()].map(row => {
         return {
-          top: Math.floor(metadata.height as number / x) * _x,
-          left: Math.floor(metadata.width as number / y) * _y,
-          width: Math.floor(metadata.width as number / y),
-          height: Math.floor(metadata.height as number / x)
+          top: Math.floor(metadata.height as number / cols) * col,
+          left: Math.floor(metadata.width as number / rows) * row,
+          width: Math.floor(metadata.width as number / rows),
+          height: Math.floor(metadata.height as number / cols)
         }
       })
     });
 
-    const result = await Promise.all(flatten(presets).map(async (p: IPreset) => await crop(idata, p)));
+    const result = await Promise.all(flatten(presets).map((p: IPreset) => crop(idata, p)));
+    await console.log("Image Fetch time (hrtime): ", hrend[1]/1000000 ,'ms');
     cb(null, buildRespose(null, "Cropped", 200));
 
   } catch (e) {
     cb(null, buildRespose(e.message, "Error", 520))
   }
 }
-
